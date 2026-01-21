978281529a97c64711059b7b2f314153594ce28e
Le0pardd <punnawit549@gmail.com>
2026-01-21 16:01:00 +0700

    Tryyyyy2

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,27 +1,22 @@
-export function run(arg: string) {
-    const N: number = parseInt(arg);
+import * as process from 'process';
 
-    if (isNaN(N) || N <= 0) {
-        return;
-    }
+const input: string = process.argv[2];
+const N: number = parseInt(input);
 
+
+if (!isNaN(N) && N > 0) {
     for (let i = 1; i <= N; i++) {
-        let numbers = "";
+        let line = "";
         
+       
         for (let j = 1; j <= i; j++) {
-            numbers += j;
+            line += j;
         }
+        
         
         const spaces = " ".repeat(N - i);
         
-        console.log(spaces + numbers);
+    
+        console.log(spaces + line);
     }
 }
-
-import * as process from 'process';
-run(process.argv[2]);
-
-
-
-
-
