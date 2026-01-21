529a4ea3d8cfd2546e3db4c17d6adf36253bdaf7
Le0pardd <punnawit549@gmail.com>
2026-01-21 15:48:56 +0700

    Newwwww20

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,20 +1,16 @@
-export function fizzbuzz(input: any) {
-    const n = Number(input);
-    if (Number.isNaN(n) || n <= 0) {
-      return;
-    }
-  
-   
-    for (let i = 1; i <= n; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-      } else if (i % 3 === 0) {
-        console.log("Fizz");
-      } else if (i % 5 === 0) {
-        console.log("Buzz");
-      } else {
-        console.log(i);
-      }
+const n =Number(process.arguments[2]);
+
+if (!isNaN(n) && n >0) {
+for (let i =1; i <= n; i++) {
+if (i %3 ===0 && i %5 ===0) {
+console.log("FizzBuzz");
+    }else if (i %3 ===0) {
+console.log("Fizz");
+    }else if (i %5 ===0) {
+console.log("Buzz");
+    }else {
+console.log(i);
     }
   }
+}
 
