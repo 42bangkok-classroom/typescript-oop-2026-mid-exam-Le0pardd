bcaa1645c45aa7e647bb35f1357c3c570e45b19b
Le0pardd <punnawit549@gmail.com>
2026-01-21 15:46:11 +0700

    New10

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,19 +1,20 @@
-const N: number = parseInt(prcess.argv[2]);
+export function printPattern(input: any) {
+    const N = parseInt(input);
 
-if (isNaN(N) || N <= 0) {
-    console.log("Please a number");
-    process.exit(1);
-}
-for (let i = 1; i <= N; i++) {
-    let line = "";
-    
-    for (let j = N; j > N - i; j--) {
-        line += j;
+    if (isNaN(N) || N <= 0) {
+        return;
     }
-    
-    const spaces = " ".repeat(N - i);
 
-    console.log(spaces + line);
+    for (let i = 1; i <= N; i++) {
+        let line = "";
+        
+        for (let j = 1; j <= i; j++) {
+            line += j;
+        }
+        T
+        const spaces = " ".repeat(N - i);
+        console.log(spaces + line);
+    }
 }
 
 
