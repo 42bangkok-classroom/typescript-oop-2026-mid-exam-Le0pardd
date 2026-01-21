9c783002a7b719d5778c2770aa552cc4f44fa8ba
Le0pardd <punnawit549@gmail.com>
2026-01-21 14:57:47 +0700

    Wowwwwwwwwww

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,22 @@
-// Write your code below
+const N: number = parseInt(prcess.argv[2]);
+
+if (isNaN(N) || N <= 0) {
+    console.log("Please a number");
+    process.exit(1);
+}
+for (let i = 1; i <= N; i++) {
+    let line = "";
+    
+    for (let j = N; j > N - i; j--) {
+        line += j;
+    }
+    
+    const spaces = " ".repeat(N - i);
+
+    console.log(spaces + line);
+}
+
+
+
+
+
