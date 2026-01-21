b9b20e6fdfaa06759a66e6d9178099b883bb8ed4
Le0pardd <punnawit549@gmail.com>
2026-01-21 16:19:35 +0700

    Cryyyyyyy3

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,16 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+
+export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+    
+    const combined = [...arr1, ...arr2];
+
+    
+    return combined.filter((num) => {
+        return combined.indexOf(num) === combined.lastIndexOf(num);
+    });
 }
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+const array1 = [1, 2, 3, 4];
+const array2 = [3, 4, 5, 6];
+
+const result = getUniqueNumbers(array1, array2);
+console.log(result);
